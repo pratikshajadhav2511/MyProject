92d8ab1eca348a9b16006a89c4f623017966d979
pratikshajadhav2511 <pratikshajadhav1225@gmail.com>
2026-09-11 13:22:36 +0000

    Conversion of Operation

---
+++ Basics/01basic.js
@@ -0,0 +1,6 @@
+const accId = 1234;
+let accEmail = "praiksha@gmail.com";
+var accPass = "1212";  //dont use var 
+let accCity = "Jaipur";
+
+console.table([accId , accEmail , accPass , accCity]); //display info in tabular form
---
+++ Basics/ConversionofOperation.js
@@ -0,0 +1,22 @@
+let score = undefined
+
+console.log(typeof score);
+
+
+let valueinNumber = Number(score)
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+
+/*
+"33" => 33
+"33abs" =>NaN
+null => Object
+true => 1
+
+let isLoggedIn = 1
+let booleanisLoggedin = Boolean(isLoggedIn)
+console.log(booleanisLoggedin);
+
+"" => false
+" Hello " => true
+*/
