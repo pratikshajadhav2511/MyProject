555fb48d2f72aa781a3bfc1250293d1266f4f004
pratikshajadhav2511 <pratikshajadhav1225@gmail.com>
2026-09-11 12:55:51 +0000

    Datatypes

---
+++ Basics/Datatype.js
@@ -0,0 +1,11 @@
+"use strict" //treate all js code as newer version
+/*
+Number => range upto 2 to power 53
+BigInt
+String => "Hello"
+null => standalone value
+undefined
+Boolean => True/False 
+Symbol => Unique
+*/
+console.log(typeof "HELLO")
